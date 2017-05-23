7e56b06f6170f3c227a69af2823c98326fc6c349
DanilovAnton <DanilovAnt.Vik@gmail.com>
2017-05-23 11:50:01 +0300

    реализация круд CRUD

--- src/main/java/com/onlyhorde/springsimpleapp/repository/InMemMealRepository.java
+++ src/main/java/com/onlyhorde/springsimpleapp/repository/InMemMealRepository.java
@@ -1,6 +1,52 @@
 package com.onlyhorde.springsimpleapp.repository;
 
+import com.onlyhorde.springsimpleapp.model.Meal;
+import com.onlyhorde.springsimpleapp.utility.MealsUtil;
 
-public class InMemMealRepository {
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+import java.util.Map;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class InMemMealRepository implements MealRepository{
+    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+//        MealsUtil.MEALSLIST.forEach(this::save); с использованием
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        save(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 490));
+    }
+
+    @Override
+    public Meal save(Meal meal) {
+        if (meal.isNew()){
+            meal.setId(counter.incrementAndGet());
+        }
+        repository.put(meal.getId(), meal);
+        return meal;
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.remove(id);
+    }
+
+    @Override
+    public Meal get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return repository.values();
+    }
 }
